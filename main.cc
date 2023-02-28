8802bbac2582c1ae54f39de712d27b917b1dab54
Sam Zofkie <samzofkie@gmail.com>
2023-02-28 16:08:32 -0500

    Nicered formattin'

--- main.cc
+++ main.cc
@@ -20,58 +20,55 @@ void err_n_exit(const char* message, int err = 1) {
   exit(err);
 }
 
-class CairoXWindow {
-  public:
-    double ww, wh;
-    Display *display;
-    int screen;
-    Drawable window;
-    cairo_surface_t *surface;
-    cairo_t *cr;
 
-    CairoXWindow(double ww_arg, double wh_arg) :
-      ww(ww_arg), wh(wh_arg)
-    {
-      if (!(display = XOpenDisplay(NULL)))
-        err_n_exit("XOpenDisplay failed");
-      screen = DefaultScreen(display);
-      window = XCreateSimpleWindow(display, DefaultRootWindow(display),
-          0, 0, ww, wh, 0, 0, 0);
-      XSelectInput(display, window, 
-          ButtonPressMask | ButtonReleaseMask |
-          KeyPressMask | KeyReleaseMask |
-          Button1MotionMask | ExposureMask);
-      XMapWindow(display, window);
-      surface = cairo_xlib_surface_create(display, window,
-          DefaultVisual(display, screen),
-          DisplayWidth(display, screen),
-          DisplayHeight(display, screen));
-      cr = cairo_create(surface);
-    }
+struct CairoXWindow {
+  double ww, wh;
+  Display *display;
+  int screen;
+  Drawable window;
+  cairo_surface_t *surface;
+  cairo_t *cr;
+
+  CairoXWindow(double ww_arg, double wh_arg) : ww(ww_arg), wh(wh_arg) {
+    if (!(display = XOpenDisplay(NULL)))
+      err_n_exit("XOpenDisplay failed");
+    screen = DefaultScreen(display);
+    window = XCreateSimpleWindow(display, DefaultRootWindow(display),
+                                 0, 0, ww, wh, 0, 0, 0);
+    XSelectInput(display, window, 
+                 ButtonPressMask | ButtonReleaseMask |
+                 KeyPressMask | KeyReleaseMask |
+                 Button1MotionMask | ExposureMask);
+    XMapWindow(display, window);
+    surface = cairo_xlib_surface_create(display, window,
+                                        DefaultVisual(display, screen),
+                                        DisplayWidth(display, screen),
+                                        DisplayHeight(display, screen));
+    cr = cairo_create(surface);
+ }
 };
 
-enum PulseStreamType {record, playback};
 
-class PulseStream {
-  public:
-    pa_simple *s;
-    const pa_sample_spec ss;
-    int error;
+/*enum PulseStreamType {record, playback};
 
-    PulseStream(const pa_sample_spec &ss_arg, PulseStreamType type) :
-      ss(ss_arg)
-    {
-      pa_stream_direction_t dir =
-        type == record? PA_STREAM_RECORD : PA_STREAM_PLAYBACK;
-      const char *stream_name = 
+struct PulseStream {
+  pa_simple *s;
+  const pa_sample_spec ss;
+  int error;
+
+  PulseStream(const pa_sample_spec &ss_arg, PulseStreamType type) : ss(ss_arg) {
+    pa_stream_direction_t dir = 
+        type == record ? PA_STREAM_RECORD : PA_STREAM_PLAYBACK;
+    const char *stream_name = 
         type == record? "app_record_stream" : "app_playback_stream";
-      if (!(s = pa_simple_new(NULL, "app_name", dir, NULL,
-              stream_name, &ss, NULL, NULL, &error))) {
-        char *err_mess = (char*)"pa_simple_new failed for ";
-        err_n_exit(strcat(err_mess, stream_name));
-      }
-    } 
-};
+    if (!(s = pa_simple_new(NULL, "app_name", dir, NULL, stream_name, &ss, NULL,
+                            NULL, &error))) {
+      char *err_mess = (char*)"pa_simple_new failed for ";
+      err_n_exit(strcat(err_mess, stream_name));
+    }
+  } 
+};*/
+
 
 struct P {
   pa_mainloop *mainloop;
@@ -79,19 +76,19 @@ struct P {
   int error;
   pa_sample_spec sample_spec;
   pa_stream *stream;
+  
   P() {
     mainloop = pa_mainloop_new();
     context = pa_context_new(pa_mainloop_get_api(mainloop), "swag daw");
-    if ((error=pa_context_connect(context, NULL, PA_CONTEXT_NOAUTOSPAWN, NULL)) < 0)
+    if ((error=pa_context_connect(context, NULL, 
+                                  PA_CONTEXT_NOAUTOSPAWN, NULL)) < 0)
       err_n_exit("pa_context_connect failed"); 
 
     int state = 0; 
     pa_context_set_state_callback(context, pa_context_state_callback, &state);
 
-    while (state==0) {
-      cout << "context aint ready" << endl;
+    while (state==0) 
       pa_mainloop_iterate(mainloop, 1, NULL);
-    }
     if (state==2)
       err_n_exit("pa_context_state_callback said it failed");
 
@@ -100,18 +97,17 @@ struct P {
       .rate = 44100,
       .channels = 2 
     };
-    if (!(stream = pa_stream_new(context, "swag daw stream", &sample_spec, NULL)))
+    if (!(stream = pa_stream_new(context, "swag daw stream", 
+                                 &sample_spec, NULL)))
       err_n_exit("pa_stream_new failed");
-    if((pa_stream_connect_record(stream, NULL, NULL, PA_STREAM_NOFLAGS))!=0)
+    if ((pa_stream_connect_record(stream, NULL, NULL, PA_STREAM_NOFLAGS))!=0)
       err_n_exit("pa_stream_connect_record failed");
     
     state = 0;
     pa_stream_set_state_callback(stream, pa_stream_state_callback, &state);
     
-    while (state==0) {
-      cout << "stream aint ready" << endl;
-      pa_mainloop_iterate(mainloop, 1, NULL);
-    }
+    while (state==0)
+      pa_mainloop_iterate(mainloop, 1, NULL); 
     if (state==2)
       err_n_exit("pa_stream_state_callback said ot failed");
 
@@ -158,7 +154,8 @@ struct P {
     }
   }
 
-  void static pa_stream_read_callback(pa_stream *s, size_t nbytes, void *userdata) {
+  void static pa_stream_read_callback(pa_stream *s, size_t nbytes, 
+                                      void *userdata) {
     cout << "pa_stream_read_callback got told " << nbytes << endl;
     size_t b;
     const void *buf;
@@ -175,14 +172,16 @@ struct P {
   }
 };
 
+
 struct Point {
   double x, y;
 };
 
+
 struct Rectangle {
   double x, y, w, h;
-  Rectangle(double _x=0, double _y=0, double _w=300, double _h=100) :
-    x(_x), y(_y), w(_w), h(_h) {}
+  Rectangle(double _x=0, double _y=0, double _w=300, double _h=100) 
+    : x(_x), y(_y), w(_w), h(_h) {}
   Rectangle(const Rectangle& _rect) :
     x(_rect.x), y(_rect.y), w(_rect.w), h(_rect.h) {}
   bool Intersects(const Point p) {
@@ -193,27 +192,38 @@ struct Rectangle {
   }
 };
 
+
 struct ClickableRectangle : public virtual Rectangle {
   using Rectangle::Rectangle;
   virtual void OnClick() =0;
 };
+
 
 struct Glyph : public virtual Rectangle {
   using Rectangle::Rectangle;
   virtual void Draw(cairo_t*) =0;
 };
 
+
+struct Live {
+  const vector<int16_t>& buf;
+  Live(const vector<int16_t>& _buf) : buf(_buf) {}
+};
+
+
 struct RecordButton : public ClickableRectangle, public Glyph {
   bool& recording;
   queue<Glyph*>& redraw_queue;
-  RecordButton(const Rectangle& _rect, bool& _recording,
-      queue<Glyph*>& _redraw_queue) :
-    Rectangle(_rect), recording(_recording), 
-    redraw_queue(_redraw_queue) {}
+
+  RecordButton(const Rectangle& _rect, bool& _recording, 
+               queue<Glyph*>& _redraw_queue)
+      : Rectangle(_rect), recording(_recording), redraw_queue(_redraw_queue) {}
+  
   void OnClick() {
     recording = !recording;
     redraw_queue.push(this);
   }
+  
   void Draw(cairo_t *cr) {
     if (recording)
       cairo_set_source_rgb(cr, 1, 0, 0);
@@ -224,19 +234,17 @@ struct RecordButton : public ClickableRectangle, public Glyph {
   }
 };
 
-struct Live {
-  const vector<int16_t>& buf;
-  Live(const vector<int16_t>& _buf) : buf(_buf) {}
-};
 
 struct WaveformViewer : public Glyph, public Live {
-  WaveformViewer(const Rectangle& _rect, const vector<int16_t>& _buf) :
-    Rectangle(_rect), Live(_buf) {}
+  WaveformViewer(const Rectangle& _rect, const vector<int16_t>& _buf) 
+      : Rectangle(_rect), Live(_buf) {}
+
 	void Draw(cairo_t *cr) {
 	  // Black out behind waveform
     cairo_set_source_rgb(cr, 0, 0, 0); 
     cairo_rectangle(cr, x-1, y, w, h);
     cairo_fill(cr);
+    
     // Draw waveform
     cairo_set_source_rgb(cr, 0, 1, 0.5); // Lovely hacker green
     cairo_move_to(cr, x, y + h / 2);
@@ -250,37 +258,40 @@ struct WaveformViewer : public Glyph, public Live {
 	}	
 };
 
-class AudioClip : public Glyph, public Live {
-  public:
-    vector<int16_t> clip;
-    uint drawn;
-    AudioClip(const Rectangle& _rect, vector<int16_t>& _buf) :
-      Rectangle(_rect), Live(_buf), 
-      clip(buf.size()), drawn(0) {}
-    void Record() {
-      memcpy(&clip.back() - (buf.size()-1), &buf.front(), buf.size());
-      clip.resize(clip.size() + buf.size());
-    }
-    void Draw(cairo_t *cr) {
-      cairo_set_source_rgb(cr, 0, 0, 0.5);
-      cairo_rectangle(cr, x+drawn, y, clip.size() / buf.size() - 1 - drawn, h);
-      cairo_fill(cr);
-      cairo_set_source_rgb(cr, 1, 1, 1);
-      while (drawn < clip.size() / buf.size() - 1) {
-        int16_t low = numeric_limits<int16_t>::max(), 
-                high = numeric_limits<int16_t>::min();
-        for (vector<int16_t>::size_type i=0; i<buf.size(); i++) {
-          int index = drawn * buf.size() + i;
-          high = clip[index] > high ? clip[index] : high;
-          low = clip[index] < low ? clip[index] : low;
-        }
-        cairo_move_to(cr, x+drawn+0.5, y + h/2 + h/2 * high/pow(2,15));
-        cairo_line_to(cr, x+drawn+0.5, y + h/2 + h/2 * low/pow(2,15));
-        cairo_stroke(cr);
-        drawn++;
+
+struct AudioClip : public Glyph, public Live {
+  vector<int16_t> clip;
+  uint drawn;
+  
+  AudioClip(const Rectangle& _rect, vector<int16_t>& _buf) 
+      : Rectangle(_rect), Live(_buf), clip(buf.size()), drawn(0) {}
+
+  void Record() {
+    memcpy(&clip.back() - (buf.size()-1), &buf.front(), buf.size());
+    clip.resize(clip.size() + buf.size());
+  }
+  
+  void Draw(cairo_t *cr) {
+    cairo_set_source_rgb(cr, 0, 0, 0.5);
+    cairo_rectangle(cr, x+drawn, y, clip.size() / buf.size() - 1 - drawn, h);
+    cairo_fill(cr);
+    cairo_set_source_rgb(cr, 1, 1, 1);
+    while (drawn < clip.size() / buf.size() - 1) {
+      int16_t low = numeric_limits<int16_t>::max(), 
+              high = numeric_limits<int16_t>::min();
+      for (vector<int16_t>::size_type i=0; i<buf.size(); i++) {
+        int index = drawn * buf.size() + i;
+        high = clip[index] > high ? clip[index] : high;
+        low = clip[index] < low ? clip[index] : low;
       }
-    } 
+      cairo_move_to(cr, x+drawn+0.5, y + h/2 + h/2 * high/pow(2,15));
+      cairo_line_to(cr, x+drawn+0.5, y + h/2 + h/2 * low/pow(2,15));
+      cairo_stroke(cr);
+      drawn++;
+    }
+  } 
 };
+
 
 int main() {
   CairoXWindow X(1250, 750);
